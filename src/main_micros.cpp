91ab554254ef08af0f868ebccf6bf32b2a1ac87d
DevM7 <39317319+DevM7@users.noreply.github.com>
2018-07-16 12:12:55 -0400

    This has the "delayMicroseconds" arduino command instead of "_delay_cycles" c command

---
+++ src/main_micros.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <DigitalIO.h>
+#include <delay_cycles_avr.h>
+#include <time.h>
+
+const int ttlpulseout = 2;
+const int cam = 4;
+int cycle;
+
+
+void setup() {
+  fastPinMode(ttlpulseout, OUTPUT);
+  fastPinMode(cam, OUTPUT);
+  cycle = 0;
+}
+void loop() {
+  fastDigitalWrite(ttlpulseout, HIGH);
+  while (cycle <= 500) {
+    fastDigitalWrite(cam, HIGH);
+    delayMicroseconds(1000);
+    fastDigitalWrite(cam, LOW);
+    delayMicroseconds(1000);
+    Serial.print(millis());
+    cycle++;
+  }
+  fastDigitalWrite(ttlpulseout, LOW);
+}
