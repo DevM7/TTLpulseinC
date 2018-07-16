2961ade4436b57ece6f095a91fd55d7ca1a408f5
Michael <michaelfromano@gmail.com>
2018-07-16 09:41:10 -0400

    added clock cycle delay counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include <DigitalIO.h>
+#include <delay_cycles_avr.h>
 
 const int ttlpulseout = 2;
 const int cam = 4;
+const int MHz = 16;
 int cycle;
 float t2, t4;
+
 
 void setup() {
   fastPinMode(ttlpulseout, OUTPUT);
@@ -16,12 +19,10 @@ void loop() {
   while (cycle <= 500) {
     t2 = micros();
     fastDigitalWrite(cam, HIGH);
-    while (micros()-t2 < 1000) {
-    }
+    _delay_cycles(1000*MHz);
     t4 = micros();
     fastDigitalWrite(cam, LOW);
-    while ((micros()-t4) < 1000) {
-    }
+    _delay_cycles(1000*MHz);
     cycle++;
   }
   fastDigitalWrite(ttlpulseout, LOW);
